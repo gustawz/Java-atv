a73cef62d81d5b53aa180ed8214c0e0951c42d65
gustawz <gustavowzff@gmail.com>
2024-09-20 20:16:35 -0300

    Add files via upload

---
+++ Atv5/src/main/java/com/mycompany/atv5/Atv5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atv5;
+
+/**
+ *
+ * @author Admin
+ */
+public class Atv5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
